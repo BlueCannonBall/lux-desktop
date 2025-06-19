4a01558379cbb8e1f70d7f68332f7971d5b076ea
BlueCannonBall <mmttiizz100@gmail.com>
2025-06-19 12:48:10 -0400

    Remove defunct theme functions from theme.hpp

--- theme.hpp
+++ theme.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <string>
 #ifdef _WIN32
     #define WIN32_LEAN_AND_MEAN
     #include <windows.h>
 #endif
 
 bool is_dark_mode();
-std::string get_gtk_theme();
 void configure_fltk_colors();
 #ifdef _WIN32
 void set_window_dark_mode(HWND window, bool value = is_dark_mode());
